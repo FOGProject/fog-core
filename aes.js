997a5f2b8b51b358217a9d1d4452c1faa6946168
Jbob <contact@jbob.io>
2015-09-08 09:22:48 -0500

    Make all aes methods output in hex format

--- aes.js
+++ aes.js
@@ -2,20 +2,16 @@ var forge = require('node-forge');
 var async = require('async');
 var aes   = {};
 
-// Encrypt a message using AES with the given padding
 var encryptSync = function(iv, key, data, padding) {
   var cipher = forge.cipher.createCipher(padding, key);
 
   cipher.start({iv: iv});
   cipher.update(forge.util.createBuffer(data));
   cipher.finish();
-  return cipher.output;
+  return cipher.output.toHex();
 }
 aes.encrypt = async.asyncify(encryptSync);
 
-// Decrypt an AES message with the given padding
-// blob contains .iv and .data
-// Returns the decrypted message
 var decryptSync = function(iv, key, data, padding) {
   var decipher   = forge.cipher.createDecipher(padding, key);
   var byteBuffer = forge.util.createBuffer(data);
@@ -29,11 +25,27 @@ var decryptSync = function(iv, key, data, padding) {
 aes.decrypt = async.asyncify(decryptSync);
 
 aes.generateKey = function(cb) {
-  forge.random.getBytes(32, cb);
+  forge.random.getBytes(32, function(err, key) {
+    if (err) {
+      cb(err);
+      return;
+    }
+
+    var hexKey = new Buffer(key, 'binary').toString('hex');
+    cb(err, hexKey);
+  });
 }
 
 aes.generateIV = function(cb) {
-  forge.random.getBytes(16, cb);
+  forge.random.getBytes(16, function(err, iv) {
+    if (err) {
+      cb(err);
+      return;
+    }
+
+    var hexIV = new Buffer(iv, 'binary').toString('hex');
+    cb(err, hexIV);
+  });
 }
 
 module.exports = aes;
