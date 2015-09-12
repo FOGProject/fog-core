caba07f92378128f73fedce6b41d6bf88b30cdd4
Jbob <contact@jbob.io>
2015-09-11 23:45:25 -0500

    Correct encode variable names

--- libraries/encode.js
+++ libraries/encode.js
@@ -8,12 +8,12 @@ encode.hexSync = function(bytes) {
 encode.hex = async.asyncify(encode.hexSync);
 
 encode.base64Sync = function(bytes) {
-  return forge.util.encode64(data);
+  return forge.util.encode64(bytes);
 };
 encode.base64 = async.asyncify(encode.base64Sync);
 
 encode.utf8Sync = function(bytes) {
-  return forge.util.encodeUtf8(data);
+  return forge.util.encodeUtf8(bytes);
 };
 encode.utf8 = async.asyncify(encode.utf8Sync);
 
